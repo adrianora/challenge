dff31268f22d21a22e9755994fedc9354a705611
adrianora <adriano.araujo@ccc.ufcg.edu.br>
2017-06-05 01:08:02 -0300

    Minimo

---
+++ minimo.py
@@ -0,0 +1,6 @@
+def searchMin(array):
+        min = array[0]
+            for element in range(0, len(array)):
+                        if element < min
+                                    min = element
+                                        return min
