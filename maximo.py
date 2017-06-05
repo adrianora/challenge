0910f03074d77037ae300bf0abfbdba216b375e2
adrianora <adriano.araujo@ccc.ufcg.edu.br>
2017-06-05 01:06:02 -0300

    Implementacao max

---
+++ maximo.py
@@ -0,0 +1,6 @@
+def searchMax(array):
+        max = array[0]
+            for element in range(0, len(array)):
+                        if element > max
+                                    max = element
+                                        return max
